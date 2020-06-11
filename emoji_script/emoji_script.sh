449e9eb5d3e518e1be06456a405edbab42526d20
cezaris13 <pijus.cezar13@gmail.com>
2020-06-11 19:18:44 +0300

    update

--- emoji_script/emoji_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-cat /home/pijus/.config/i3/emoji_script/rez.txt | dmenu -i -l 20 -fn Monospace-15 | awk '{print $1}' | tr -d '\n' | xclip -selection clipboard
-if [ ! -z "$(xclip -o -selection clipboard)" ]
-then
-    notify-send "$(xclip -o -selection clipboard) copied to clipboard."
-fi
---
+++ lock.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -e
+xset s off dpms 0 10 0
+i3lock -i /home/pijus/Pictures/wallpapers/bsod.png -u
+xset s off -dpms
